b06d4a81b3ae00c28959f886fbbae701f068446f
Vumba798 <alexandrov32649@gmail.com>
2020-10-17 15:12:59 +0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <utility>
+#include <memory>
+#include <utility>
+
+using std::endl;
+using std::cin;
+using std::cout;
+using std::string;
+
+template<typename T>
+T move(T&& oth) {
+    return --oth;
+}
+
+void foo(const string s) {
+
+}
+template<typename T>
+T& constanter(T& val) {
+    return val;
+}
+
+template <typename... Types, typename... Args>
+class tuple {
+private:
+
+public:
+    
+
+};
+
+int main() {
+    string str = "foo string";
+    string&& newStr = "bar string";
+    cout << newStr << endl;
+    cout << str << endl;
+    return 0;
+}
